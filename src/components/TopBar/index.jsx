c3486630eaf5e545bc6e7671f336296205b590ae
DatNguyen1512 <thodat1512@gmail.com>
2025-04-19 17:04:38 +0700

    Update index.jsx

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -29,7 +29,7 @@ function TopBar() {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {}
         <Typography variant="h6" color="inherit">
-          Phạm Trung Hiếu
+          Nguyễn Thọ Đạt
         </Typography>
 
         {}
